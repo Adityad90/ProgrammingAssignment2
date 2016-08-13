2f03dd937bd5c11e0016763eb0fe26809987da15
Adityad90 <adityadhote@gmail.com>
2016-08-13 14:05:17 +0530

    Adding 2 functions to complete the assgnment
    Assignment: Caching the Inverse of a Matrix.
    1.makeCacheMatrix
    2.CacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates a special vector which stores object and functions to fetch and store it
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## Creates a list which contains function and object to get and fetch the matrix and it's inverse if exists.
+makeCacheMatrix <- function(x = matrix())
+{
+   inverse <- NULL
+   set <- function(y)
+	{
+	  x <<- y
+	  inverse <- NULL
+	}
+   #primitive function
+   #returns x
+   get <- function() x 
+   #Does Inverse of Matrix provided as argument 
+   setinverse <- function(resolve) inverse <<- solve
+   #get Inverse of matrix 
+   getinverse <- function() inverse
+   # Assign all these functions to named list 
+   list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+ }
 
+
+## It checks whether matrix in a particular list has computed inverse, if not exist, calculate inverse and store it in list.
+
+cacheSolve <- function(x,..)
+{
+   # Find in Vector if inverse exists
+   inverse <-x$getinverse()
+   # if not found compute and store it.
+   if(!is.null(inverse))
+	{
+	  message("Inverse exists in Cache")
+	  return(inverse)
+	}
+   data <- x$get()
+   inverse <- solve(data)
+   x$setinverse(inverse)
+   inverse
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
 }
